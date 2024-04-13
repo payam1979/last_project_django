9cb0579067bf087d3649561e40b3e1252d8238fd
Payam <bagheri.payam@gmail.com>
2024-04-13 22:07:06 +0330

    some changes before deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yvrxjn8b(%l)onl%%h@cf@-9335dwgr3$bvht&&jkl$n4lc&t7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -55,8 +46,6 @@ INSTALLED_APPS = [
     
 ]
 
-#sites framework
-SITE_ID = 2
 
 #robots
 ROBOTS_USE_HOST = False
@@ -132,15 +121,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -178,14 +159,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
 
 
 # Default primary key field type
@@ -199,7 +175,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-X_FRAME_OPTIONS='SAMEORIGIN'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
@@ -210,4 +185,3 @@ EMAIL_HOST_PASSWORD = 'otasdkgkcfsxtmio' #your password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
